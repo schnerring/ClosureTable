4fd18b8a0833cf715aad6be14a2eef9f7e032bd8
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2023-03-27 05:20:12 +0200

    Add roots() queryable to context

--- src/ClosureTable.Infrastructure/ClosureContext.cs
+++ src/ClosureTable.Infrastructure/ClosureContext.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ClosureTable.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ClosureTable.Infrastructure;
 
-public class ClosureContext : DbContext
+public class ClosureContext<TEntity, TKey> : DbContext
+    where TEntity : SelfReferencingEntity<TEntity, TKey>
+    where TKey : notnull
 {
+    public IQueryable<TEntity> Roots => Set<TEntity>().Where(entity => entity.ParentId == null);
 }
