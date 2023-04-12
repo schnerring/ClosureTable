ebaab71f59f923e5d202d21ca10d6d805880663a
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2023-04-13 00:02:02 +0200

    Add DbContext extensions: HasDescendants, DescendantsCount

--- src/ClosureTable.Infrastructure.Tests/TestEntityExtensions.cs
+++ src/ClosureTable.Infrastructure.Tests/TestEntityExtensions.cs
@@ -46,6 +46,64 @@ public static class TestEntityExtensions
     }
 
     private static Expression<Func<AncestorDescendantRelationship<TEntity, TKey>, bool>>
+        DescendantsPredicate<TEntity, TKey>(TKey id)
+        where TEntity : SelfReferencingEntity<TEntity, TKey>
+        where TKey : struct
+    {
+        return relationship => relationship.AncestorId.Equals(id) && relationship.Depth > 0;
+    }
+
+    public static bool HasDescendants<TEntity, TKey>(
+        this DbContext @this,
+        TKey id)
+        where TEntity : SelfReferencingEntity<TEntity, TKey>
+        where TKey : struct
+    {
+        return @this
+            .Set<AncestorDescendantRelationship<TEntity, TKey>>()
+            .AsNoTracking()
+            .Any(DescendantsPredicate<TEntity, TKey>(id));
+    }
+
+    public static async Task<bool> HasDescendantsAsync<TEntity, TKey>(
+        this DbContext @this,
+        TKey id,
+        CancellationToken cancellationToken = default)
+        where TEntity : SelfReferencingEntity<TEntity, TKey>
+        where TKey : struct
+    {
+        return await @this
+            .Set<AncestorDescendantRelationship<TEntity, TKey>>()
+            .AsNoTracking()
+            .AnyAsync(DescendantsPredicate<TEntity, TKey>(id), cancellationToken);
+    }
+
+    public static int DescendantsCount<TEntity, TKey>(
+        this DbContext @this,
+        TKey id)
+        where TEntity : SelfReferencingEntity<TEntity, TKey>
+        where TKey : struct
+    {
+        return @this
+            .Set<AncestorDescendantRelationship<TEntity, TKey>>()
+            .AsNoTracking()
+            .Count(DescendantsPredicate<TEntity, TKey>(id));
+    }
+
+    public static async Task<int> DescendantsCountAsync<TEntity, TKey>(
+        this DbContext @this,
+        TKey id,
+        CancellationToken cancellationToken = default)
+        where TEntity : SelfReferencingEntity<TEntity, TKey>
+        where TKey : struct
+    {
+        return await @this
+            .Set<AncestorDescendantRelationship<TEntity, TKey>>()
+            .AsNoTracking()
+            .CountAsync(DescendantsPredicate<TEntity, TKey>(id), cancellationToken);
+    }
+
+    private static Expression<Func<AncestorDescendantRelationship<TEntity, TKey>, bool>>
         AncestorsPredicate<TEntity, TKey>(TKey id)
         where TEntity : SelfReferencingEntity<TEntity, TKey>
         where TKey : struct
--- src/ClosureTable.Infrastructure.Tests/TestsBase.cs
+++ src/ClosureTable.Infrastructure.Tests/TestsBase.cs
@@ -61,6 +61,138 @@ public abstract partial class TestsBase<TFixture> : IClassFixture<TFixture>, IAs
     [InlineData("F", false)]
     [InlineData("G", false)]
     [InlineData("H", false)]
+    [InlineData("I", true)]
+    [InlineData("J", true)]
+    [InlineData("K", true)]
+    [InlineData("L", false)]
+    [InlineData("M", false)]
+    [InlineData("N", false)]
+    [InlineData("O", false)]
+    public void HasDescendants_OfTestDataEntity_ShouldBeExpected(string entityName, bool expected)
+    {
+        // Arrange
+        using var context = _fixture.CreateContext();
+
+        var sut = context
+            .TestEntities
+            .AsNoTracking()
+            .First(entity => entity.Name == entityName);
+
+        // Act
+        var actual = context.HasDescendants<TestEntity, Guid>(sut.Id);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("A", false)]
+    [InlineData("B", false)]
+    [InlineData("C", false)]
+    [InlineData("D", false)]
+    [InlineData("E", false)]
+    [InlineData("F", false)]
+    [InlineData("G", false)]
+    [InlineData("H", false)]
+    [InlineData("I", true)]
+    [InlineData("J", true)]
+    [InlineData("K", true)]
+    [InlineData("L", false)]
+    [InlineData("M", false)]
+    [InlineData("N", false)]
+    [InlineData("O", false)]
+    public async Task HasDescendantsAsync_OfTestDataEntity_ShouldBeExpected(string entityName, bool expected)
+    {
+        // Arrange
+        await using var context = _fixture.CreateContext();
+
+        var sut = context
+            .TestEntities
+            .AsNoTracking()
+            .First(entity => entity.Name == entityName);
+
+        // Act
+        var actual = await context.HasDescendantsAsync<TestEntity, Guid>(sut.Id);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("A", 0)]
+    [InlineData("B", 0)]
+    [InlineData("C", 0)]
+    [InlineData("D", 0)]
+    [InlineData("E", 0)]
+    [InlineData("F", 0)]
+    [InlineData("G", 0)]
+    [InlineData("H", 0)]
+    [InlineData("I", 6)]
+    [InlineData("J", 2)]
+    [InlineData("K", 1)]
+    [InlineData("L", 0)]
+    [InlineData("M", 0)]
+    [InlineData("N", 0)]
+    [InlineData("O", 0)]
+    public void DescendantsCount_OfTestDataEntity_ShouldBeExpected(string entityName, int expected)
+    {
+        // Arrange
+        using var context = _fixture.CreateContext();
+
+        var sut = context
+            .TestEntities
+            .AsNoTracking()
+            .First(entity => entity.Name == entityName);
+
+        // Act
+        var actual = context.DescendantsCount<TestEntity, Guid>(sut.Id);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("A", 0)]
+    [InlineData("B", 0)]
+    [InlineData("C", 0)]
+    [InlineData("D", 0)]
+    [InlineData("E", 0)]
+    [InlineData("F", 0)]
+    [InlineData("G", 0)]
+    [InlineData("H", 0)]
+    [InlineData("I", 6)]
+    [InlineData("J", 2)]
+    [InlineData("K", 1)]
+    [InlineData("L", 0)]
+    [InlineData("M", 0)]
+    [InlineData("N", 0)]
+    [InlineData("O", 0)]
+    public async Task DescendantsCountAsync_OfTestDataEntity_ShouldBeExpected(string entityName, int expected)
+    {
+        // Arrange
+        await using var context = _fixture.CreateContext();
+
+        var sut = context
+            .TestEntities
+            .AsNoTracking()
+            .First(entity => entity.Name == entityName);
+
+        // Act
+        var actual = await context.DescendantsCountAsync<TestEntity, Guid>(sut.Id);
+
+        // Assert
+        actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData("A", false)]
+    [InlineData("B", false)]
+    [InlineData("C", false)]
+    [InlineData("D", false)]
+    [InlineData("E", false)]
+    [InlineData("F", false)]
+    [InlineData("G", false)]
+    [InlineData("H", false)]
     [InlineData("I", false)]
     [InlineData("J", true)]
     [InlineData("K", true)]
